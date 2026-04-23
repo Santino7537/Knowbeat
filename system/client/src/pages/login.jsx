13b54776dfd562c36314d8fe6483837000bbb12c
EstebanGonzalez20 <gonzavenom@gmail.com>
2026-04-23 15:24:02 -0300

    Crea la página de incio de sesión y sus componentes respectivos

---
+++ system/client/src/pages/login.jsx
@@ -0,0 +1,12 @@
+import Form_card from "../components/login-register/form_card.jsx"
+
+function Login(){
+
+    return(
+        <>
+            <Form_card title = "Hola"/>
+        </>
+    )
+}
+
+export default Login
