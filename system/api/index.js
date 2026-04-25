34abd985698b0c525516b0225dbfea65604ae53d
Santino7537 <martinez.santinonicolaset36@gmail.com>
2026-04-24 21:24:42 -0300

    Agrega un index.js con un package.json con cors y express

---
+++ system/api/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+const PORT = 3000;
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000'
+]; // Origenes permitidos para CORS
+
+server.use(express.json({ limit: '100kb' })); // Limita el tamaño del body a 100kb
+server.use(express.urlencoded({ limit: '100kb', extended: true })); // Limita el tamaño del body a 100kb para datos codificados en URL
+
+server.use(cors({ // No permite solicitudes de otros orígenes que no estén en la lista de allowedOrigins
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) { // Se puede agregar "!origin ||" para testeos 
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+server.get('/', (req, res) => {
+    res.status(200).send('Bienvenido a la API de Knowbeat!');
+});
+
+server.listen(PORT, async () => {
+    console.log('La API está corriendo en el puerto ', PORT);
+})
