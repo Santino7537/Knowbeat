7a7421f93de654f5f6214d011284be8358031e44
Renaaa189 <renatagallucci3@gmail.com>
2026-04-29 15:22:39 -0300

    Agrega el middleware 'checkToken' y se agrega en ciertos endpoints

--- system/api/index.js
+++ system/api/index.js
@@ -4,9 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const { login, register, getUsers, deleteUser } = require('./controllers/userController');
 const isAdmin = require('./middlewares/isAdmin');
+const checkToken = require('./middlewares/checkToken');
 
 const server = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   'http://localhost:5173',
@@ -18,7 +19,7 @@ server.use(express.urlencoded({ limit: '100kb', extended: true })); // Limita el
 
 server.use(cors({ // No permite solicitudes de otros orígenes que no estén en la lista de allowedOrigins
   origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin)) { // Se puede agregar "!origin ||" para testeos 
+    if (!origin || allowedOrigins.includes(origin)) { // Se puede agregar "!origin ||" para testeos 
       callback(null, true);
     } else {
       callback(new Error('No permitido por CORS'));
@@ -34,16 +35,16 @@ server.get('/', (req, res) => {
     res.status(200).send('Bienvenido a la API de Knowbeat!');
 });
 
-server.post('/login', login)
-server.post('/register', register)
+server.post('/login', login);
+server.post('/register', register);
 
-server.get('/users', getUsers);
+server.get('/users', checkToken, getUsers);
 
 // DELETE /users/:id (Momentaneo)
 // En HEADERS x-user-id: 1 (para testing, se tiene que cambiar despues)
 
-server.delete('/delUsers/:id', isAdmin, deleteUser);
+server.delete('/delUsers/:id', checkToken, isAdmin, deleteUser);
 
 server.listen(PORT, async () => {
     console.log('La API está corriendo en el puerto ', PORT);
-})
+});
