fde8871eabb84d18fe128183d34ae93f14348ba4
Santino7537 <martinez.santinonicolaset36@gmail.com>
2026-06-03 05:10:45 -0300

    Modifica archivo incluir los campos nulos

--- system/api/middlewares/binnacleHelper.js
+++ system/api/middlewares/binnacleHelper.js
@@ -1,7 +1,7 @@
-const { ComputeDVHFromObject } = require('../utils/dvhHelpers');
+const { ComputeDVHFromObject } = require("../utils/dvhHelpers");
 
 const PostResponseLog = (req, res, next) => {
-  if (req.method === 'OPTIONS') return next(); // Ignoramos preflights
+  if (req.method === "OPTIONS") return next(); // Ignoramos preflights
 
   const start = process.hrtime.bigint(); // Tiempo para saber duración de respuesta
 
@@ -14,45 +14,50 @@ const PostResponseLog = (req, res, next) => {
     return oldJson.call(this, body); // Seguimos el comportamiento original
   };
 
-  res.once('finish', async () => {
+  res.once("finish", async () => {
     const durationMs = Number(process.hrtime.bigint() - start) / 1_000_000;
 
-    const sessionToken = req.user?.sessionToken
+    const sessionToken = req.user?.sessionToken;
     const user_id = req.user?.user_id;
     const actions = req.actions_data;
-    const ip = req.ip || req.headers['x-forwarded-for']?.split(',')[0] || req.connection.remoteAddress
-    const { headers, params, query, body } = req
+    const ip =
+      req.ip ||
+      req.headers["x-forwarded-for"]?.split(",")[0] ||
+      req.connection.remoteAddress;
+    const { headers, params, query, body } = req;
 
     try {
       const binnacle_data = {
         actions: actions,
         endpoint_route: req.path,
+        ip_source: ip || null,
+        user_id: user_id || null,
+        session_token: sessionToken || null,
         request_data: { headers },
         response_data: {
-            status_code: res.statusCode,
-            response_time: durationMs,
-            body: responseBody
+          status_code: res.statusCode,
+          response_time: durationMs,
+          body: responseBody,
         },
-        timestamp: new Date()
+        timestamp: new Date(),
       };
 
-      ip && (binnacle_data.ip_source = ip);
-      sessionToken && (binnacle_data.session_token = sessionToken);
-      user_id && (binnacle_data.user_id = user_id);
       params && (binnacle_data.request_data.params = params);
       query && (binnacle_data.request_data.query = query);
       body && (binnacle_data.request_data.body = body);
 
       binnacle_data.dvh = ComputeDVHFromObject(binnacle_data);
 
-      await db.query('INSERT INTO Binnacle (actions, endpoint_route, ip_source, user_id, session_token, request_data, response_data, timestamp, dvh) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);',
-      Object.values(binnacle_data));
+      await db.query(
+        "INSERT INTO Binnacle (actions, endpoint_route, ip_source, user_id, session_token, request_data, response_data, timestamp, dvh) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);",
+        Object.values(binnacle_data),
+      );
     } catch (err) {
-      console.error('binnacle persistence failed', err);
+      console.error("binnacle persistence failed", err);
     }
   });
 
   next();
-}
+};
 
-module.exports = { PostResponseLog }
+module.exports = { PostResponseLog };
